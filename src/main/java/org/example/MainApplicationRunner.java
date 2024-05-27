b74aa48b2e61604792d6d1e1889ea1bc6b53e8da
michal-bisikiewicz <bisikiewiczmichal@gmail.com>
2024-05-28 00:27:56 +0200

    added privat final to fields

--- src/main/java/org/example/MainApplicationRunner.java
+++ src/main/java/org/example/MainApplicationRunner.java
@@ -1,21 +1,23 @@
 package org.example;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import org.example.itunes.service.ItunesService;
 import org.example.sampleshawnmendes.service.ShawnMendesService;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MainApplicationRunner {
-    ItunesService itunesService;
-    ShawnMendesService shawnMendesService;
+    private final ItunesService itunesService;
+    private final ShawnMendesService shawnMendesService;
 
+    //robimy tutaj wstrzykiwanie przez konstruktor, także adnotacja @Autowired nie jest potrzebna
     public MainApplicationRunner(ItunesService itunesService, ShawnMendesService shawnMendesService) {
         this.itunesService = itunesService;
         this.shawnMendesService = shawnMendesService;
     }
 
-    public void run() {
+    public void run() throws JsonProcessingException {
          itunesService.fetchShawnMendesSongs();
-//         shawnMendesService.testClient();
+         shawnMendesService.testClient();
      }
 }

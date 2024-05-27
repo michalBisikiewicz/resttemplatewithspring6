c8d7ebade36e003119999597aac517c0b5ba0e47
michal-bisikiewicz <bisikiewiczmichal@gmail.com>
2024-05-28 00:25:42 +0200

    added jackson dependency for object mapping

--- src/main/java/org/example/itunes/service/ItunesMapper.java
+++ src/main/java/org/example/itunes/service/ItunesMapper.java
@@ -1,16 +1,15 @@
 package org.example.itunes.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.example.itunes.proxy.ItunesResponse;
 import org.springframework.stereotype.Component;
-
-import java.util.List;
 
 @Component
 public class ItunesMapper {
 
-    ItunesResponse mapJsonItunesResponse(String json) {
-        return new ItunesResponse(0, List.of());
-//        ObjectMapper objectMapper = new ObjectMapper();
-//        return objectMapper.readValue(json, ItunesResponse.class);
+    ItunesResponse mapJsonItunesResponse(String json) throws JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        return objectMapper.readValue(json, ItunesResponse.class);
     }
 }
--- src/main/java/org/example/sampleshawnmendes/service/ShawnMendesServiceMapper.java
+++ src/main/java/org/example/sampleshawnmendes/service/ShawnMendesServiceMapper.java
@@ -1,18 +1,18 @@
 package org.example.sampleshawnmendes.service;
 
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.example.sampleshawnmendes.proxy.SampleServerShawnMendesResponse;
 import org.springframework.stereotype.Component;
-
-import java.util.List;
 
 @Component
 public class ShawnMendesServiceMapper {
 
-    SampleServerShawnMendesResponse mapJsonToSampleShawnMendesResponse(String json) {
-        System.out.println("mapJsonToSampleShawnMendesResponse");
-        return new SampleServerShawnMendesResponse("1", List.of());
-//        ObjectMapper objectMapper = new ObjectMapper();
-//        return objectMapper.readValue(json, SampleServerShawnMendesResponse.class);
+    SampleServerShawnMendesResponse mapJsonToSampleShawnMendesResponse(String json) throws JsonProcessingException {
+//        System.out.println("mapJsonToSampleShawnMendesResponse");
+//        return new SampleServerShawnMendesResponse("1", List.of());
+        ObjectMapper objectMapper = new ObjectMapper();
+        return objectMapper.readValue(json, SampleServerShawnMendesResponse.class);
     }
 }

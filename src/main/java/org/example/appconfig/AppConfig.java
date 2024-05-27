d73403ba15ff05fdf126de6f00aa5a846a7e0db1
michal-bisikiewicz <bisikiewiczmichal@gmail.com>
2024-05-28 01:00:49 +0200

    added ObjectMapper Bean

--- src/main/java/org/example/appconfig/AppConfig.java
+++ src/main/java/org/example/appconfig/AppConfig.java
@@ -1,5 +1,6 @@
 package org.example.appconfig;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
@@ -14,5 +15,10 @@ public class AppConfig {
         configurer.setLocation(new ClassPathResource("application.properties"));
         return configurer;
     }
+    @Bean
+    public ObjectMapper objectMapper() {
+        return new ObjectMapper();
+    }
+
 
 }

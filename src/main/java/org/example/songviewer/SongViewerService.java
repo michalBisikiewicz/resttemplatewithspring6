e214dee9b4015d56517144fdc080082d570e562c
michal-bisikiewicz <bisikiewiczmichal@gmail.com>
2024-05-28 22:23:22 +0200

    refactored viewAllSongs method

--- src/main/java/org/example/songviewer/SongViewerService.java
+++ src/main/java/org/example/songviewer/SongViewerService.java
@@ -1,6 +1,5 @@
 package org.example.songviewer;
 
-import org.example.itunes.proxy.ItunesResult;
 import org.example.itunes.service.ItunesService;
 import org.example.sampleshawnmendes.service.ShawnMendesService;
 import org.springframework.stereotype.Service;
@@ -21,16 +20,11 @@ public class SongViewerService {
     }
 
     public List<Song> viewAllSongs() {
+        List<Song> itunesResults = itunesService.fetchAllSongs();
+        List<Song> songs = shawnMendesService.fetchAllSongs();
         List<Song> songsToView = new ArrayList<>();
-        List<ItunesResult> itunesResults = itunesService.fetchShawnMendesSongs();
-        String songs = shawnMendesService.fetchAllShawnMendesSongsFromLocalHost();
-
-        itunesResults.forEach(
-                itunesResult -> songsToView.add(new Song(itunesResult.trackName()))
-        );
-        //na razie traktujemy songs jako pojedyńczego stringa
-        songsToView.add(new Song(songs));
-
+        songsToView.addAll(itunesResults);
+        songsToView.addAll(songs);
         return songsToView;
     }
 

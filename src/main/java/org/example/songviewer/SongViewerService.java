742d3c4e56967cefec88c48967a023ae2e5ffaca
michal-bisikiewicz <bisikiewiczmichal@gmail.com>
2024-05-31 18:38:08 +0200

    changed name of one field

--- src/main/java/org/example/songviewer/SongViewerService.java
+++ src/main/java/org/example/songviewer/SongViewerService.java
@@ -9,16 +9,16 @@ import java.util.List;
 public class SongViewerService {
 
     //dzięki List<SongFetchable> będzie wstrzykiwana cała lista serwisów używająca tego Interfejsu
-    private final List<SongFetchable> songFetchable;
+    private final List<SongFetchable> services;
 
-    public SongViewerService(List<SongFetchable> songFetchable) {
-        this.songFetchable = songFetchable;
+    public SongViewerService(List<SongFetchable> services) {
+        this.services = services;
     }
 
     public List<Song> viewAllSongs() {
         List<Song> songsToView = new ArrayList<>();
-        songFetchable.forEach(
-                songService -> songsToView.addAll(songService.fetchAllSongs())
+        services.forEach(
+                service -> songsToView.addAll(service.fetchAllSongs())
         );
         return songsToView;
     }
